02f3bd8bd798912b1d975f8573efe17157bbf463
Robert-Main <mainrobert04@gmail.com>
2026-06-30 15:57:17 +0300

    initial set up

---
+++ StockReview/Program.cs
@@ -0,0 +1,24 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
